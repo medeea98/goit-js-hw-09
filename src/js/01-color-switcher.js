c1c9fae950dbeb1448bc6644d8689dda60d45d01
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-01-27 00:26:45 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const start = document.querySelector("[data-start]")
+const stop = document.querySelector("[data-stop]")
+let colorChange = null;
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+start.addEventListener('click', function() {
+    if (colorChange !== null) {
+        return;}
+    
+    colorChange = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
 
+    this.disabled = true; 
+} )
+  stop.addEventListener('click', function() {
+    clearInterval(colorChange);
+    colorChange = null;
+    startButton.disabled = false; 
+});
