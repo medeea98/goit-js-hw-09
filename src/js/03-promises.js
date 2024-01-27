194473e7ae4af74b446e5b7cf532e607435b0b6f
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-01-27 13:40:50 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,23 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+document.querySelector('.form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const delay = parseInt(this.delay.value, 10);
+  const step = parseInt(this.step.value, 10);
+  const amount = parseInt(this.amount.value, 10);
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay + step * (i - 1))
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+});
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      Math.random() > 0.3 ? resolve({ position, delay }) : reject({ position, delay });
+    }, delay);
+  });
 }
